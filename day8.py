c29e44e772ac92700464967aea66e24013105fba
Berken Tekin <tekin.berken01@gmail.com>
2021-12-08 13:32:50 +0100

    Small optimization

--- day8.py
+++ day8.py
@@ -1,13 +1,10 @@
 def main() -> None:
     with open("8_input.txt", "r") as f:
         result = 0
-        for line in f.readlines():
-            strings = line.split(" | ")
-            output = strings[1].strip().split(" ")
-            for out in output:
-                if len(out) in (2, 4, 3, 7):
-                    result += 1
-        print(result)
+        for line in filter(lambda x: x != '\n', f.readlines()):
+            for out in line.split(" | ")[1].strip().split(" "):
+                result += 1 if len(out) in (2, 3, 4, 7) else 0
+    print(result)
 
 
 if __name__ == "__main__":
